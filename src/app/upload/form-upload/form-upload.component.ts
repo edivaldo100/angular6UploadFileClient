31aefc6f31beb84baf27565970d2ad1f7b962065
Edivaldo <edivaldo100@gmail.com>
2019-05-29 00:22:28 -0300

    implementação de filtros para upload de arquivos

--- src/app/upload/form-upload/form-upload.component.ts
+++ src/app/upload/form-upload/form-upload.component.ts
@@ -11,6 +11,7 @@ export class FormUploadComponent implements OnInit {
 
   selectedFiles: FileList;
   currentFileUpload: File;
+  currentFileUploadTemp: File;
   progress: { percentage: number } = { percentage: 0 };
 
   constructor(private uploadService: UploadFileService) { }
@@ -19,7 +20,40 @@ export class FormUploadComponent implements OnInit {
   }
 
   selectFile(event) {
+
+
     this.selectedFiles = event.target.files;
+    console.log(event);
+    this.currentFileUploadTemp = this.selectedFiles.item(0);
+    console.log(this.currentFileUploadTemp.name);
+    console.log(this.currentFileUploadTemp.size);
+    console.log(this.currentFileUploadTemp.type);
+
+    var descNOME = this.currentFileUploadTemp.name;
+    var descTAMANHO = this.currentFileUploadTemp.size;
+    var descTIPO = this.currentFileUploadTemp.type;
+
+    var desc = "NOME : "+descNOME+"<br /> TAMANHO : "+descTAMANHO+" <br />TIPO : "+descTIPO;
+
+
+
+    if(descTIPO == "application/pdf" && descTAMANHO < 10000100){
+      document.getElementById('descriptionsFile').style.display = 'block';
+      document.getElementById('descriptionsFile').innerHTML = desc;
+      document.getElementById('msgErro').style.display = 'none';
+      document.getElementById('bt').style.display = 'block';
+
+    }else{
+
+
+      document.getElementById('bt').style.display = 'none';
+      document.getElementById('descriptionsFile').style.display = 'none';
+      document.getElementById('msgErro').style.display = 'block';
+      document.getElementById('msgErro').innerHTML = desc+" <br /> <br /> <br />ERRO ao Enviar o arquivo! Pois é do tipo invalido ou ultrapassa o tamanho máximo permitido.";
+
+    }
+
+
   }
 
   upload() {
